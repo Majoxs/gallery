60448fe47bf60d0bb8964d1f2814919cb029db35
Majoxs <majoxs@gmail.com>
2022-06-22 14:36:31 +0500

    Added API unsplash

---
+++ scripts/const.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://api.unsplash.com';
+export const API_URL_PHOTOS = `${API_URL}/photos`;
+export const ACCESS_KEY = 't62lWQnP7JwrjwivGvTTEhqW3JDoslfIw74AnMrGyDQ';
+export const SECRET_KEY = 'atuMQh4C_aVVRAbu9qM89I5WPoZlttaoTIsaszGqnkA';
